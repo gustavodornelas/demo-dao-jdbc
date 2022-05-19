ea9962d01a0ab27006dcbf62b24391401baca10c
“gustavodornelas” <dornelas.gustavoptc@gmail.com>
2022-05-19 12:11:06 -0300

    Delete Seller implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -95,8 +95,24 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
+			st.setInt(1, id);
+			int rows = st.executeUpdate();
+			
+			if (rows != 0) {
+				System.out.println("Delete completed");
+			}else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+			
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
